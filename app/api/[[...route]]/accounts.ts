d3e7cacd24a0df6f0d12f6cbb08130d1398dffc3
mstout60 <mstout@eprospector.com>
2024-06-06 18:30:28 -0500

    Added Global Confirmation Hook

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,7 +1,9 @@
+import { z } from "zod";
+
 import { db } from "@/db/drizzle";
 import { accounts, insertAccountSchema } from "@/db/schema";
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
-import { eq } from "drizzle-orm";
+import { and, eq, inArray, } from "drizzle-orm";
 import { Hono } from "hono";
 import { zValidator } from "@hono/zod-validator";
 import { createId } from "@paralleldrive/cuid2";
@@ -49,5 +51,37 @@ const app = new Hono()
 
             return c.json({ data });
         })
+    .post(
+        "/bulk-delete",
+        clerkMiddleware(),
+        zValidator(
+            "json",
+            z.object({
+                ids: z.array(z.string())
+            }),
+        ),
+        async (c) => {
+            const auth = getAuth(c);
+            const values = c.req.valid("json");
+
+            if (!auth?.userId) {
+                return c.json({ error: "Unauthorized" }, 401);
+            }
+
+            const data = await db
+                .delete(accounts)
+                .where(
+                    and(
+                        eq(accounts.userId, auth.userId),
+                        inArray(accounts.id, values.ids)
+                    )
+                )
+                .returning({
+                    id: accounts.id
+                });
+
+            return c.json({ data });
+        },
+    );
 
 export default app;
