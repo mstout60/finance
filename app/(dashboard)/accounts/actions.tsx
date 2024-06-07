99697ded29d3a13d950c17d06305091f5217aebd
mstout60 <mstout@eprospector.com>
2024-06-06 22:33:10 -0500

    Added single delete for account

--- app/(dashboard)/accounts/actions.tsx
+++ app/(dashboard)/accounts/actions.tsx
@@ -9,17 +9,33 @@ import {
     DropdownMenuItem,
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { Edit, MoreHorizontal } from "lucide-react";
+import { Edit, MoreHorizontal, Trash } from "lucide-react";
+import { useDeleteAccount } from "@/features/accounts/api/use-delete-account";
+import { useConfirm } from "@/hooks/use-confirm";
 
 type Props = {
     id: string;
 }
 
 export const Actions = ({ id }: Props) => {
+    const [ConfirmDialog, confirm] = useConfirm(
+        "Are you sure?",
+        "You are about to delete this account."
+    );
+    const deleteMutation = useDeleteAccount(id);
     const { onOpen } = useOpenAccount();
+
+    const handleDelete = async () => {
+        const ok = await confirm();
+
+        if (ok) {
+            deleteMutation.mutate();
+        }
+    };
 
     return (
         <>
+        <ConfirmDialog />
             <DropdownMenu>
                 <DropdownMenuTrigger asChild>
                     <Button variant="ghost" className="size-8 p-0">
@@ -28,11 +44,18 @@ export const Actions = ({ id }: Props) => {
                 </DropdownMenuTrigger>
                 <DropdownMenuContent align="end">
                     <DropdownMenuItem
-                        disabled={false}
+                        disabled={deleteMutation.isPending}
                         onClick={() => { onOpen(id)}}
                     >
                         <Edit className="size-4 mr-2" />
                         Edit
+                    </DropdownMenuItem>
+                    <DropdownMenuItem
+                        disabled={deleteMutation.isPending}
+                        onClick={handleDelete}
+                    >
+                        <Trash className="size-4 mr-2" />
+                        Delete
                     </DropdownMenuItem>
                 </DropdownMenuContent>
             </DropdownMenu>
--- features/accounts/components/edit-account-sheet.tsx
+++ features/accounts/components/edit-account-sheet.tsx
@@ -14,6 +14,9 @@ import { useOpenAccount } from "@/features/accounts/hooks/use-open-accounts";
 import { useGetAccount } from "@/features/accounts/api/use-get-account";
 import { Loader2 } from "lucide-react";
 import { useEditAccount } from "@/features/accounts/api/use-edit-account";
+import { useDeleteAccount } from "@/features/accounts/api/use-delete-account";
+
+import { useConfirm } from "@/hooks/use-confirm";
 
 const formSchema = insertAccountSchema.pick({
     name: true,
@@ -24,12 +27,20 @@ type FormValues = z.input<typeof formSchema>;
 export const EditAccountSheet = () => {
     const { isOpen, onClose, id } = useOpenAccount();
 
+    const [ConfirmDialog, confirm] = useConfirm(
+        "Are you sure?",
+        "You are about to delete this account."
+    );
+
     const accountQuery = useGetAccount(id);
     const editMutation = useEditAccount(id);
+    const deleteMutation = useDeleteAccount(id);
+
     const isLoading = accountQuery.isLoading;
 
     const isPending =
-        editMutation.isPending
+        editMutation.isPending ||
+        deleteMutation.isPending;
 
     const onSubmit = (values: FormValues) => {
         editMutation.mutate(values, {
@@ -39,6 +50,18 @@ export const EditAccountSheet = () => {
         });
     };
 
+    const onDelete = async () => {
+        const ok = await confirm();
+
+        if (ok) {
+            deleteMutation.mutate(undefined, {
+                onSuccess: () => {
+                    onClose();
+                }
+            });
+        }
+    };
+
     const defaultValues = accountQuery.data ? {
         name: accountQuery.data.name,
     } : {
@@ -46,31 +69,35 @@ export const EditAccountSheet = () => {
     }
 
     return (
-        <Sheet open={isOpen} onOpenChange={onClose}>
-            <SheetContent className="space-y-4">
-                <SheetHeader>
-                    <SheetTitle>
-                        Edit Account
-                    </SheetTitle>
-                    <SheetDescription>
-                        Edit an existing account.
-                    </SheetDescription>
-                </SheetHeader>
-                {isLoading
-                    ? (
-                        <div className="absolute inset-0 flex items-center justify-center">
-                            <Loader2 className="size-4 text-muted-foreground animate-spin" />
-                        </div>
-                    ) : (
-                        <AccountForm
-                            id={id}
-                            onSubmit={onSubmit}
-                            disabled={isPending}
-                            defaultValues={defaultValues}
-                        />
-                    )
-                }
-            </SheetContent>
-        </Sheet>
+        <>
+            <ConfirmDialog />
+            <Sheet open={isOpen} onOpenChange={onClose}>
+                <SheetContent className="space-y-4">
+                    <SheetHeader>
+                        <SheetTitle>
+                            Edit Account
+                        </SheetTitle>
+                        <SheetDescription>
+                            Edit an existing account.
+                        </SheetDescription>
+                    </SheetHeader>
+                    {isLoading
+                        ? (
+                            <div className="absolute inset-0 flex items-center justify-center">
+                                <Loader2 className="size-4 text-muted-foreground animate-spin" />
+                            </div>
+                        ) : (
+                            <AccountForm
+                                id={id}
+                                onSubmit={onSubmit}
+                                disabled={isPending}
+                                defaultValues={defaultValues}
+                                onDelete={onDelete}
+                            />
+                        )
+                    }
+                </SheetContent>
+            </Sheet>
+        </>
     );
 };
